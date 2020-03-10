823dedd294c170aa429d7160a1e8ee24a4a033ba
Pietro <pietro.morerio@gmail.com>
2020-03-11 00:43:02 +0100

    mer 11 mar 2020, 00.43.02, CET

--- plot.py
+++ plot.py
@@ -27,7 +27,7 @@ matplotlib.rcParams['font.family'] = "sans-serif"
 
 with plt.xkcd():
 	#~ fig = plt.figure()
-    p = plt.plot([day_data['data'].split()[0] for day_data in regional_data],[int(day_data['totale_casi']) for day_data in regional_data])
+    p = plt.plot([day_data['data'].split()[0] for day_data in regional_data[1:]],[int(day_data['totale_casi']) for day_data in regional_data])
     plt.setp(p, color='r', linewidth=2.0, marker='o')
     plt.xticks(rotation=45)
     #plt.show()
