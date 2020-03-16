cf01bedf4d3a6ea3f22c89bacae82c5f15b33feb
Pietro Morerio <pietro.morerio@gmail.com>
2020-03-16 20:16:42 +0100

    asd

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 cd 
 cd code/covid_project/COVID-19 && git pull && cd ../covid-19-liguria
 python plot.py
+cd italy
+python plot_italy.py
+cd ..
 MESSAGE=$(date)
 git add plots/*
 git commit -m "$MESSAGE"
